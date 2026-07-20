3ff7e38b767b4c75fe4e02c8c6cf11cbd63cd20a
muneerkhalid1121-web <muneerkhalid1121@gmail.com>
2026-07-20 20:03:57 +0300

    fix: add puppeteer args to fix infinite loading during QR scan in Docker

--- index.js
+++ index.js
@@ -53,7 +53,16 @@ io.on('connection', (socket) => {
             authStrategy: new LocalAuth(),
             puppeteer: {
                 headless: true,
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
+                args: [
+                    '--no-sandbox', 
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--disable-accelerated-2d-canvas',
+                    '--no-first-run',
+                    '--no-zygote',
+                    '--single-process',
+                    '--disable-gpu'
+                ]
             }
         });
 
